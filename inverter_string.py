c98b65528c1ef41769b2d88f3c5253d0af172d27
Carina Figueiredo <135866605+Carinavif@users.noreply.github.com>
2024-04-14 11:49:31 -0300

    Add files via upload

---
+++ inverter_string.py
@@ -0,0 +1,7 @@
+def inverter_string(string):
+    return string[::-1]
+
+# Exemplo de utilização
+string_original = input("Digite uma string para inverter: ")
+string_invertida = inverter_string(string_original)
+print("A string invertida é:", string_invertida)

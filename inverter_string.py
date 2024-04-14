ac41aae21315308c009f30d867e7046e4d94b091
Carina Figueiredo <135866605+Carinavif@users.noreply.github.com>
2024-04-14 12:08:47 -0300

    inverter_string.py

--- inverter_string.py
+++ inverter_string.py
@@ -1,11 +1,7 @@
 def inverter_string(string):
-    # Inicializamos uma string vazia para armazenar a string invertida
     string_invertida = ""
-    # Percorremos a string de trás para frente
     for i in range(len(string) - 1, -1, -1):
-        # Concatenamos cada caractere na string invertida
         string_invertida += string[i]
-    # Retornamos a string invertida
     return string_invertida
 
 # Exemplo na prática
